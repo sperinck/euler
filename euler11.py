6daf4eafb386c43b3b78283b448d10ce218e2019
Fahad Sperinck <55352599+sperinck@users.noreply.github.com>
2025-10-15 06:35:34 +0100

    Create euler11.py

--- euler11.py
+++ euler11.py
@@ -22,8 +22,6 @@ data = """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"""
 
-
-
 rows = data.split('\n')
 
 M = []
@@ -75,5 +73,3 @@ for i in range(3,20):
 
 print(largest)
 print("--- %s seconds ---" % (time.time() - start_time))
-#Runs a fraction of a second quicker than my other code on Anders
-#because I'm not keeping track of every product anymore.
